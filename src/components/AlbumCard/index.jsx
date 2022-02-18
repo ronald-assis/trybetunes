b65cb6542c266f91281576a9843cba846d4b60fb
Ronald Assis <ronaldassis71@gmail.com>
2022-02-18 15:31:00 -0300

    Adjust styles AlbumList and AlbumCard components

--- src/components/AlbumCard/index.jsx
+++ src/components/AlbumCard/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import './AlbumCard.css';
 
 export default class AlbumCard extends Component {
   render() {
